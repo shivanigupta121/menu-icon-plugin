d7cfe296c9090fc00bae7ac421a1a5fa555d44ef
Shivani Gupta <113524961+shivanigupta121@users.noreply.github.com>
2024-02-29 09:50:19 +0530

    Update class-public.php

--- menu-icon-plugin/public/class-public.php
+++ menu-icon-plugin/public/class-public.php
@@ -4,14 +4,23 @@ add_filter('wp_nav_menu_objects', 'navbar_menu_icon', 10, 2);
 function navbar_menu_icon($items, $args) {
     foreach ($items as $item) {
         $icon_url = get_post_meta($item->ID, '_menu_item_icon', true);
+
         if (!empty($icon_url)) {
-            // Check if the item is a parent or child
-            $icon_image = '<img src="' . esc_url($icon_url) . '" alt="Menu Icon" style="width: 24px; margin-right: 5px;" />';
-            $item->title = '<a href="' . esc_url($item->url) . '" style="display: flex; align-items: center;">' . $icon_image . $item->title . '</a>';
+            // Get the file extension
+            $file_extension = pathinfo($icon_url, PATHINFO_EXTENSION);
+        
+            if ($file_extension === 'svg') {
+
+                $icon_image = file_get_contents(esc_url($icon_url));
+                $icon_image = str_replace('<svg', '<svg width="24" height="24"', $icon_image);
+            } else {
+                $icon_image = '<img src="' . esc_url($icon_url) . '" alt="Menu Icon" style="width: 24px; />';
+            }
+        
+            $item->title = '<a href="' . esc_url($item->url) . '" style="display: flex; align-items: center;">' . $icon_image . '<span style="margin-left: 5px;">' . $item->title . '</span></a>';
         }
+        
+        
     }
     return $items;
 }
-
-
-
